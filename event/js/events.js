5b8891fbddb9a18995cad53f97e0a01e582a3b95
KAnti-VP <144044786+KAnti-VP@users.noreply.github.com>
2023-11-08 13:24:12 +0100

    Update events.js

--- event/js/events.js
+++ event/js/events.js
@@ -1,18 +1,20 @@
 const button = document.querySelector(".btn")
 
 // Event listener
-button.addEventListener("click",
-
-function() {
+button.addEventListener("click",function() {
 console.log("első event listener")
 })
+
 button.addEventListener("click", () => {
 console.log("második event listener")
 })
+
+
 // Event handler
 button.onclick = () => {
 console.log("első event handler")
 }
+
 button.onclick = () => {
 console.log("második event handler")
 }
